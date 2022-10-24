62cd2752fac44a22bef440f340145a235e55996d
king-open <kingwang.0624@gmail.com>
2022-10-24 13:43:51 +0800

    添加 nav样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -16,13 +16,11 @@
 
 <style lang="scss" scoped>
   .nav-wrapper {
-    border: 1px solid greenyellow;
     display: flex;
     flex-direction: column;
     height: 100vh;
   }
   .content {
-    border: 1px solid blue;
     overflow: auto;
     flex-grow: 1;
   }
