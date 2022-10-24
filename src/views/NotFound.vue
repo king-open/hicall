d93c6de079835687a38cf718e64ca85df37f7700
king-open <kingwang.0624@gmail.com>
2022-10-24 11:03:20 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>当前页面不存在，请输入正确的网址</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
